8dd7e02547f5267465ca5d786f31ae589048225b
3dot14 <1718882345@qq.com>
2026-01-12 23:44:36 +0800

    参照60112日志部分进行修正。

--- src/core/http_client.py
+++ src/core/http_client.py
@@ -1,23 +1,18 @@
 import requests
 
 class HTTPClient:
-#用__init__方便测试
+    #用__init__方便测试
     def __init__(self):
-        self.response = None
-        self.headers = {}#TODO:必要性现在还不知道。
-#在HTTPClient函数中定义http_get方法
-    def http_get(self,get_url):
-        try:
-            self.response = requests.get(get_url)
-            self.response.raise_for_status()
-            return self.response.text
-#抛出异常处理，方便后期错误定位
-        except requests.exceptions.RequestException as e:
-            print(f"请求失败：{e}")
+        self.headers = {}#自定义请求头
+    #将headers合并到**kwargs中
+    def get(self,url,**kwargs):
+        if self.headers:
+            self.headers.update(kwargs)
+        kwargs['headers'] = self.headers
 
-#测试函数
-if __name__ == "__main__":
-    http_client = HTTPClient()
-    url = "http://httpbin.org/get"
-    response = http_client.http_get(url)
-    print(response)
+        try:
+            response = requests.get(url,**kwargs)
+            return response #必须要返回一个值
+        except requests.exceptions.RequestException:
+            #暂时抛出异常，以后进行调整
+            raise
